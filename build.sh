1446d25d6c6fec70d8c6206906a735f3c9813af6
team-rc-tech <thisisteamrc@gmail.com>
2025-03-23 14:56:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@
 pip install --upgrade pip
 # Extract the .zip file
 unzip rasa-chatbot.zip -d .
-# Navigate into the rasa-deploy folder
-cd rasa-deploy/
 # Install dependencies
 pip install -r requirements.txt
